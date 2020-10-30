4f7731db680ac1d004651db3fb5d12501f440e6e
OlukaDenis <olukadeno@gmail.com>
2020-10-31 01:35:55 +0300

    Add signup

--- src/views/auth/RegisterView.js
+++ src/views/auth/RegisterView.js
@@ -13,9 +13,15 @@ import {
   Typography,
   makeStyles
 } from '@material-ui/core';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Page from 'src/components/Page';
-import { signUpUser } from '../../store/actions';
+import { firebaseApp } from '../../firebase';
 import { SignUpSchema } from '../../utils';
+import AppAlerts from '../../components/AppAlerts';
+import { showAlert } from '../../store/actions/alertActions';
+import {
+  setLogIn, loggedInUser, authLoading, authError
+} from '../../store/actions/authActions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,7 +37,7 @@ const RegisterView = () => {
   const navigate = useNavigate();
 
   const authReducer = useSelector((state) => state.authReducer);
-  const { isLoggedIn } = authReducer;
+  const { error, loading, isLoggedIn } = authReducer;
   const dispatch = useDispatch();
 
   const navigateToDashboard = () => {
@@ -63,10 +69,20 @@ const RegisterView = () => {
   };
 
   const submitForm = (values) => {
-    console.log(values);
-    dispatch(signUpUser());
-    console.log(authReducer);
-    navigateToDashboard();
+    dispatch(authLoading());
+    const { email, password } = values;
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+        dispatch(loggedInUser(result.user));
+        dispatch(dispatch(setLogIn()));
+        navigateToDashboard();
+      })
+      .catch((err) => {
+        dispatch(authError(err.message));
+        dispatch(showAlert());
+        console.log(err);
+      });
   };
 
   useEffect(() => {
@@ -76,162 +92,167 @@ const RegisterView = () => {
   }, []);
 
   return (
-    <Page
-      className={classes.root}
-      title="Register"
-    >
-      <Box
-        display="flex"
-        flexDirection="column"
-        height="100%"
-        justifyContent="center"
-      >
-        <Container maxWidth="sm">
-          <Formik
-            initialValues={initialValues}
-            validationSchema={SignUpSchema}
-            validate={validate}
-            onSubmit={submitForm}
+    <div>
+      { loading && <LinearProgress color="secondary" /> }
+      {(
+        <Page
+          className={classes.root}
+          title="Register"
+        >
+          <Box
+            display="flex"
+            flexDirection="column"
+            height="100%"
+            justifyContent="center"
           >
-            {({
-              errors,
-              handleBlur,
-              handleChange,
-              handleSubmit,
-              isSubmitting,
-              touched,
-              values
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <Box mb={3}>
-                  <Typography
-                    color="textPrimary"
-                    variant="h2"
-                  >
-                    Create new account
-                  </Typography>
-                  <Typography
-                    color="textSecondary"
-                    gutterBottom
-                    variant="body2"
-                  >
-                    Use your email to create new Shuppa account
-                  </Typography>
-                </Box>
-                <TextField
-                  error={Boolean(touched.firstName && errors.firstName)}
-                  fullWidth
-                  helperText={touched.firstName && errors.firstName}
-                  label="First name"
-                  margin="normal"
-                  name="firstName"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.firstName}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.lastName && errors.lastName)}
-                  fullWidth
-                  helperText={touched.lastName && errors.lastName}
-                  label="Last name"
-                  margin="normal"
-                  name="lastName"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.lastName}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.email && errors.email)}
-                  fullWidth
-                  helperText={touched.email && errors.email}
-                  label="Email Address"
-                  margin="normal"
-                  name="email"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="email"
-                  value={values.email}
-                  variant="outlined"
-                />
-                <TextField
-                  error={Boolean(touched.password && errors.password)}
-                  fullWidth
-                  helperText={touched.password && errors.password}
-                  label="Password"
-                  margin="normal"
-                  name="password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  type="password"
-                  value={values.password}
-                  variant="outlined"
-                />
-                <Box
-                  alignItems="center"
-                  display="flex"
-                  ml={-1}
-                >
-                  <Checkbox
-                    checked={values.policy}
-                    name="policy"
-                    onChange={handleChange}
-                  />
-                  <Typography
-                    color="textSecondary"
-                    variant="body1"
-                  >
-                    I have read the
-                    {' '}
-                    <Link
-                      color="primary"
-                      component={RouterLink}
-                      to="#"
-                      underline="always"
-                      variant="h6"
+            <Container maxWidth="sm">
+              <Formik
+                initialValues={initialValues}
+                validationSchema={SignUpSchema}
+                validate={validate}
+                onSubmit={submitForm}
+              >
+                {({
+                  errors,
+                  handleBlur,
+                  handleChange,
+                  handleSubmit,
+                  touched,
+                  values
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Box mb={3}>
+                      <Typography
+                        color="textPrimary"
+                        variant="h2"
+                      >
+                        Create new account
+                      </Typography>
+                      <Typography
+                        color="textSecondary"
+                        gutterBottom
+                        variant="body2"
+                      >
+                        Use your email to create new Shuppa account
+                      </Typography>
+                    </Box>
+                    <TextField
+                      error={Boolean(touched.firstName && errors.firstName)}
+                      fullWidth
+                      helperText={touched.firstName && errors.firstName}
+                      label="First name"
+                      margin="normal"
+                      name="firstName"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.firstName}
+                      variant="outlined"
+                    />
+                    <TextField
+                      error={Boolean(touched.lastName && errors.lastName)}
+                      fullWidth
+                      helperText={touched.lastName && errors.lastName}
+                      label="Last name"
+                      margin="normal"
+                      name="lastName"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.lastName}
+                      variant="outlined"
+                    />
+                    <TextField
+                      error={Boolean(touched.email && errors.email)}
+                      fullWidth
+                      helperText={touched.email && errors.email}
+                      label="Email Address"
+                      margin="normal"
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      type="email"
+                      value={values.email}
+                      variant="outlined"
+                    />
+                    <TextField
+                      error={Boolean(touched.password && errors.password)}
+                      fullWidth
+                      helperText={touched.password && errors.password}
+                      label="Password"
+                      margin="normal"
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      type="password"
+                      value={values.password}
+                      variant="outlined"
+                    />
+                    <Box
+                      alignItems="center"
+                      display="flex"
+                      ml={-1}
                     >
-                      Terms and Conditions
-                    </Link>
-                  </Typography>
-                </Box>
-                {Boolean(touched.policy && errors.policy) && (
-                  <FormHelperText error>
-                    {errors.policy}
-                  </FormHelperText>
+                      <Checkbox
+                        checked={values.policy}
+                        name="policy"
+                        onChange={handleChange}
+                      />
+                      <Typography
+                        color="textSecondary"
+                        variant="body1"
+                      >
+                        I have read the
+                        {' '}
+                        <Link
+                          color="primary"
+                          component={RouterLink}
+                          to="#"
+                          underline="always"
+                          variant="h6"
+                        >
+                          Terms and Conditions
+                        </Link>
+                      </Typography>
+                    </Box>
+                    {Boolean(touched.policy && errors.policy) && (
+                      <FormHelperText error>
+                        {errors.policy}
+                      </FormHelperText>
+                    )}
+                    <Box my={2}>
+                      <Button
+                        color="primary"
+                        disabled={loading}
+                        fullWidth
+                        size="large"
+                        type="submit"
+                        variant="contained"
+                      >
+                        Sign up now
+                      </Button>
+                    </Box>
+                    <Typography
+                      color="textSecondary"
+                      variant="body1"
+                    >
+                      Have an account?
+                      {' '}
+                      <Link
+                        component={RouterLink}
+                        to="/login"
+                        variant="h6"
+                      >
+                        Sign in
+                      </Link>
+                    </Typography>
+                  </form>
                 )}
-                <Box my={2}>
-                  <Button
-                    color="primary"
-                    disabled={isSubmitting}
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                  >
-                    Sign up now
-                  </Button>
-                </Box>
-                <Typography
-                  color="textSecondary"
-                  variant="body1"
-                >
-                  Have an account?
-                  {' '}
-                  <Link
-                    component={RouterLink}
-                    to="/login"
-                    variant="h6"
-                  >
-                    Sign in
-                  </Link>
-                </Typography>
-              </form>
-            )}
-          </Formik>
-        </Container>
-      </Box>
-    </Page>
+              </Formik>
+            </Container>
+          </Box>
+        </Page>
+      )}
+      { error && <AppAlerts message="error" error={error} />}
+    </div>
   );
 };
 
